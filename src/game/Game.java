3104d0f078f7a33b5bff50d1fc9037567c2a45b0
Valo <valo1@web.de>
2013-03-12 19:57:47 +0100

    initial

---
+++ src/game/Game.java
@@ -0,0 +1,15 @@
+package game;
+
+/**
+ *
+ * @author V
+ */
+public class Game {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
